d27c93f97bc5bc6fa50ef3cf65a2879f2d08c35a
Recep İnanç <recepinanc96@gmail.com>
2015-10-14 23:24:12 +0300

    Splash screen initalization -- Need drawable addition Login system -- Warning text should be reviewed

---
+++ app/src/main/java/com/example/recepinanc/whenfreedeneme/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.example.recepinanc.whenfreedeneme;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by recepinanc on 14/10/15.
+ */
+public class SplashScreen extends Activity {
+
+    private static int SPLASH_TIME = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen_layout);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        }, SPLASH_TIME);
+    }
+}
